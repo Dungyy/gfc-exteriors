29ab5c8abccc0c778129163ae32e5f6486bb8df7
Dungyy <erickmunoz13@gmail.com>
2025-04-07 00:48:55 -0500

    fixed moblie layout

--- src/components/Contact/BusinessHours.js
+++ src/components/Contact/BusinessHours.js
@@ -1,17 +1,23 @@
 'use client'
 
+import React from 'react'
 import { businessHours } from './constants'
+import { FaClock } from 'react-icons/fa'
 
 const BusinessHours = () => {
+
     return (
-        <div className="mt-8 pt-8 border-t border-gray-700">
-            <h4 className="text-white font-semibold mb-4">Business Hours</h4>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-2 text-sm">
+        <div className="mt-8 pt-8 border-t border-gray-700 max-w-md">
+            <h4 className="text-white font-semibold mb-4 flex items-center">
+                <FaClock className="text-gfc-gold mr-2" />
+                Business Hours
+            </h4>
+            <div className="grid grid-cols-2 gap-x-4 gap-y-2 text-sm">
                 {businessHours.map((item, index) => (
-                    <div key={item.day || index} className="flex justify-between sm:block">
+                    <React.Fragment key={item.day || index}>
                         <div className="text-gray-300">{item.day}</div>
-                        <div className="text-right text-gray-300">{item.hours}</div>
-                    </div>
+                        <div className="text-gray-300">{item.hours}</div>
+                    </React.Fragment>
                 ))}
             </div>
         </div>
